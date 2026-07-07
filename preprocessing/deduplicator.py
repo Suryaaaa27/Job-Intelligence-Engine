d499ad44b9293ebe420bf11cbdc331d0283cd755
Surya Kumar Srivastava <srivastavasurya0111@gmail.com>
2026-07-07 09:04:42 +0530

    Update remove_duplicates to use Job model
    Refactor remove_duplicates function to use Job model attributes for deduplication.

--- preprocessing/deduplicator.py
+++ preprocessing/deduplicator.py
@@ -1,16 +1,24 @@
-def remove_duplicates(jobs):
+from scraper.models import Job
+
+
+def remove_duplicates(jobs: list[Job]):
 
     unique_jobs = []
     seen = set()
 
     for job in jobs:
 
-        key = (
+        # First preference: unique job URL
+        if job.job_url:
+            key = ("url", job.job_url.lower())
 
-         job.title.lower(),
-
-         job.location.lower()
-        )
+        # Fallback
+        else:
+            key = (
+                job.company_name.lower(),
+                job.job_title.lower(),
+                job.location.lower()
+            )
 
         if key not in seen:
             seen.add(key)
